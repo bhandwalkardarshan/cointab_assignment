b0a23e28dea32f8ddfcb7eba8d0e018f366ef1a7
bhandwalkardarshan <bhandwalkardarshan@gmail.com>
2024-02-22 14:23:25 +0530

    cors url updated

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,23 @@ const app = express();
 const userRoutes = require('./routes/userRoutes');
 
 app.use(express.json());
+app.use(cors());
 // Define a whitelist array of allowed origins
-const whitelist = ['https://cointab-assignment-tan.vercel.app','http://localhost:5173'];
+// const whitelist = ['https://cointab-assignment-tan.vercel.app','http://localhost:5173'];
 
 // Configure CORS to dynamically set the allowed origin
-app.use(cors({
-    origin: function (origin, callback) {
-        // Check if the incoming origin is in the whitelist
-        if (!origin || whitelist.includes(origin)) {
-            // Allow the request
-            callback(null, true);
-        } else {
-            // Deny the request
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // Check if the incoming origin is in the whitelist
+//         if (!origin || whitelist.includes(origin)) {
+//             // Allow the request
+//             callback(null, true);
+//         } else {
+//             // Deny the request
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     }
+// }));
 
 
 app.use('/', userRoutes);
