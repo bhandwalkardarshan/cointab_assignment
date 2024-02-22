215c54115912219ad8a6f9f05ebaf568aa3752c8
bhandwalkardarshan <bhandwalkardarshan@gmail.com>
2024-02-22 11:38:53 +0530

    cors dynamic allocation

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,21 @@ const app = express();
 const userRoutes = require('./routes/userRoutes');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+        // Define an array of allowed origins
+        const allowedOrigins = ['http://localhost:5173', 'http://example2.com'];
+
+        // Check if the incoming origin is in the array of allowed origins
+        if (!origin || allowedOrigins.includes(origin)) {
+            // Allow the request
+            callback(null, true);
+        } else {
+            // Deny the request
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 
 app.use('/', userRoutes);
 
